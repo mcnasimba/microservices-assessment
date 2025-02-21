6071cf3daf7a16b969d583b6ad1c332bc4df5c49
mcnasimba <andycrisyc@gmail.com>
2025-02-21 11:55:18 -0500

    Create microservices template

---
+++ msvc-accounts/src/main/java/com/mcnasimba/msvc_accounts/MsvcAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.mcnasimba.msvc_accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAccountsApplication.class, args);
+	}
+
+}
---
+++ msvc-clients/src/test/java/com/mcnasimba/msvc_clients/MsvcClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcnasimba.msvc_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
